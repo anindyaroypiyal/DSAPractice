2acb25a00fe0fd19bace8104f9dffe7a6c729471
Anindya Roy Piyal <anindya.roy.piyal@g.bracu.ac.bd>
2023-07-10 00:01:28 +0600

    .

--- binary searching/fibo.py
+++ binary searching/fibo.py
@@ -5,3 +5,21 @@ def fibo(n):
         return fibo(n-1) + fibo(n-2)
     
 print(fibo(6))
+
+def towerOfHanoi(n, source, destination, aux):
+    if n == 1:
+        print("Move disk 1 from source", source, "to destination", destination)
+        return
+    towerOfHanoi(n-1, source, aux, destination)
+    print("Move disk", n, "from source", source, "to destination", destination)
+    towerOfHanoi(n-1, aux, destination, source)
+
+towerOfHanoi(3, 'A', 'C', 'B')
+
+def nthTerm(n):
+    if n == 1:
+        return 1
+    else:
+        return nthTerm(n-1) + 2 * n - 1
+    
+print(nthTerm(4))
