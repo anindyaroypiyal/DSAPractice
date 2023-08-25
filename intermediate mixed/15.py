a4ba87f1e6b92ce56191120a611cfa082a957f4b
Anindya Roy Piyal <anindya.roy.piyal@g.bracu.ac.bd>
2023-08-25 09:39:03 +0600

    .

---
+++ intermediate mixed/15.py
@@ -0,0 +1,27 @@
+def sorted_insert(stack,p):
+
+    if not stack or stack[-1]<p:
+
+        stack.append(p)
+
+        return
+
+    s = stack.pop()
+
+    sorted_insert(stack,p)
+
+    stack.append(s)
+
+ 
+
+def sortStack(stack):
+
+    if not stack:
+
+        return
+
+    p = stack.pop()
+
+    sortStack(stack)
+
+    sorted_insert(stack,p)
