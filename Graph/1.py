3b6ecf245cd0900cc419cba82dd37dbc7230e32f
Anindya Roy Piyal <anindya.roy.piyal@g.bracu.ac.bd>
2023-10-06 21:28:54 +0600

    .

---
+++ Graph/1.py
@@ -0,0 +1,30 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def isGraphTree(n: int, edgeList: list):
+    # Write your code here.
+    if not n:
+        return True
+    
+    adj = {i:[] for i in range(n)}
+    for n1, n2 in edgeList:
+        adj[n1].append(n2)
+        adj[n2].append(n1)
+
+    visit = set()
+    def dfs(i, prev):
+        if i in visit:
+            return False
+
+        visit.add(i)
+        for j in adj[i]:
+            if j == prev:
+                continue
+            if not dfs(j, i):
+                return False
+        
+        return True
+
+    return dfs(0,-1) and n == len(visit)
